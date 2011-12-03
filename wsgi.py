abbd8a142676a0930621c47fcaf76f761679b85b
Michał Kłujszo <michal.klujszo@10clouds.com>
2011-12-03 14:23:12 +0100

    wsgi should have the same sys path root as local

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'oekaki.settings.stable'
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'oekaki'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.stable'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
